59abcb1d07dd20d7e742d1e22b62f4047c8759f9
romansparta <chenwill98@gmail.com>
2019-01-17 12:10:12 -0500

    Fixed small error

--- deleteCache.bat
+++ deleteCache.bat
@@ -3,7 +3,7 @@ For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
 For /f "usebackq" %%m in (`dir /b C:\Users`) do (
 Del /q /f /s "C:\Users\%%m\AppData\Local\Google\Chrome\User Data\Default\Cache\*"
-Del /q /f /s "C:\Users\%%m\AppData\Local\Google\Chrome\User Data\Default\CPUCache\*"
+Del /q /f /s "C:\Users\%%m\AppData\Local\Google\Chrome\User Data\Default\GPUCache\*"
 Del /q /f /s "C:\Users\%%m\AppData\Local\Google\Chrome\User Data\Default\Media Cache\*"
 )
 echo Cleared cache at %mydate%_%mytime% >> log.txt
