d3d4160046824c6ffd94528efc80a14b291a574d
romansparta <chenwill98@gmail.com>
2019-01-17 13:18:04 -0500

    Added more destinations to clean

--- deleteCache.bat
+++ deleteCache.bat
@@ -3,10 +3,13 @@ REM Sets the time
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
 REM Cycles through each user and deletes the specified folders
-For /f "usebackq" %%m in (`dir /b C:\Users`) do (
+For /f "usebackq" %%m in (`Dir /b C:\Users`) do (
 Del /q /f /s "C:\Users\%%m\AppData\Local\Google\Chrome\User Data\Default\Cache\*"
 Del /q /f /s "C:\Users\%%m\AppData\Local\Google\Chrome\User Data\Default\GPUCache\*"
 Del /q /f /s "C:\Users\%%m\AppData\Local\Google\Chrome\User Data\Default\Media Cache\*"
+Del /q /f /s "C:\Users\%%m\AppData\Local\Google\Chrome\User Data\Default\Application Cache\*"
+Del /q /f /s "C:\Users\%%m\AppData\Local\Microsoft\Terminal Server Client\Cache\*"
+Rmdir /s /q "C:\Users\%%m\AppData\Local\Google\Chrome\User Data\Default\File System\*"
 Del /q /f /s "C:\Users\%%m\AppData\Local\CrashDumps\*"
 Del /q /f /s "C:\Users\%%m\Downloads\*"
 REM Clears everything on desktop except shortcuts
