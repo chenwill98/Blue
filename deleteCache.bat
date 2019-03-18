fe8e70187bd635b7d904bfbb68b955f684eca455
chenwill98 <chenwill98@gmail.com>
2019-03-18 14:05:33 -0400

    Testing changes to deal with errors that arise when running the script

--- deleteCache.bat
+++ deleteCache.bat
@@ -4,16 +4,18 @@ For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
 REM Cycles through each user and deletes the specified folders
 For /f "usebackq" %%m in (`Dir /b C:\Users`) do (
-Del /q /f /s "C:\Users\%%m\AppData\Local\Google\Chrome\User Data\Default\Cache\*"
-Del /q /f /s "C:\Users\%%m\AppData\Local\Google\Chrome\User Data\Default\GPUCache\*"
-Del /q /f /s "C:\Users\%%m\AppData\Local\Google\Chrome\User Data\Default\Media Cache\*"
-Del /q /f /s "C:\Users\%%m\AppData\Local\Google\Chrome\User Data\Default\Application Cache\*"
-Del /q /f /s "C:\Users\%%m\AppData\Local\Microsoft\Terminal Server Client\Cache\*"
-Rmdir /s /q "C:\Users\%%m\AppData\Local\Google\Chrome\User Data\Default\File System\*"
-Del /q /f /s "C:\Users\%%m\AppData\Local\CrashDumps\*"
-Del /q /f /s "C:\Users\%%m\Downloads\*"
+Del /q /f /s /y "C:\Users\%%m\AppData\Local\Google\Chrome\User Data\Default\Cache\*"
+Del /q /f /s /y "C:\Users\%%m\AppData\Local\Google\Chrome\User Data\Default\GPUCache\*"
+Del /q /f /s /y "C:\Users\%%m\AppData\Local\Google\Chrome\User Data\Default\Media Cache\*"
+Del /q /f /s /y "C:\Users\%%m\AppData\Local\Google\Chrome\User Data\Default\Application Cache\*"
+Del /q /f /s /y "C:\Users\%%m\AppData\Local\Microsoft\Terminal Server Client\Cache\*"
+Rmdir /s /q /y "C:\Users\%%m\AppData\Local\Google\Chrome\User Data\Default\File System\*"
+Del /q /f /s /y "C:\Users\%%m\AppData\Local\CrashDumps\*"
+Attrib -r "C:\Users\%%m\Downloads\*" /s
+Attrib -r "C:\Users\%%m\Desktop\*" /s
+Del /q /f /s /y "C:\Users\%%m\Downloads\*"
 REM Clears everything on desktop except shortcuts
-For /f "delims=" %%F in ('Dir /b "C:\Users\%%m\Desktop" ^| findstr /vile ".lnk"') do Del "C:\Users\%%m\Desktop\%%F"
+For /f /y "delims=" %%F in ('Dir /b "C:\Users\%%m\Desktop" ^| findstr /vile ".lnk"') do Del "C:\Users\%%m\Desktop\%%F"
 )
 REM Writes time and task into log
 echo Cleared cache at %mydate%_%mytime% >> log.txt
